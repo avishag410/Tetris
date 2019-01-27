69dfda55246d9fc93b6addadd17b7f9a63b34f65
Avishag <avishag212@gmail.com>
2019-01-27 14:42:22 +0200

    before refactor - done basic block moves

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ class boardHTMLElements {
         this.gameBoardElementId = gameBoardElementId
         this.blockElementType = blockElementType
         this.blockClass = blockClass 
-
-        this.demoBlock = document.createElement(this.blockElementType) 
-        this.demoBlock.classList.add(blockClass)
-        
+        this.rowBlocksNum = 13
+        this.colBlockNum = 15
     }
 
     createBlockElement() {
@@ -26,17 +24,33 @@ class boardHTMLElements {
     }
 
     get blockWidthPx() {
-        //return document.getElementsByClassName(this.blockClass)[0].clientWidth
-        return window.getComputedStyle(document.getElementById("demoBlock"))
+        return this.boardWidthPx/this.rowBlocksNum
     }
 
     get blockHeigthPx() {
-        return document.getElementsByClassName(this.blockClass)[0].clientHeight
+        return this.boardHeigthPx/this.colBlockNum
     }
 
-    addBlock(block) {
-        // this.board.appendChild(block)
+    moveBlock(block, x, y) {
+        console.log(`x: ${x} y: ${y} this.blockWidthPx: ${this.blockWidthPx}`)
+        block.style.left = x * this.blockWidthPx + 'px'
+        block.style.top = y * this.blockHeigthPx + 'px'
 
+
+    }
+
+    addBlock(x, y) {
+        let blockElem = document.createElement("div")
+        blockElem.className = this.blockClass
+        
+        this.board.appendChild(blockElem)
+        this.moveBlock(blockElem, x, y)
+
+        return blockElem
+    }
+
+    removeBlock(block) {
+        this.board.removeChild(block)
     }
 
 }
@@ -45,61 +59,74 @@ class boardHTMLElements {
 
 class boardBlocksGrid {
     constructor(boardHTMLElements) {
-        // const board = document.getElementById("game_board")
-        // const boardWidthPx = board.style.width
-        // const boardHeigthPx = board.style.height
-    
-        // let block = document.createElement("div")
-        // block.className = "block_shape" 
-        // const blockWidthPx = block.style.width
-        // const blockHeigthPx = block.style.height
-        
-        const boardWidthBlocks = boardHTMLElements.boardWidthPx() / boardHTMLElements.blockWidthPx()
-        const boardHeigthBlocks = boardHTMLElements.boardHeigthPx() / boardHTMLElements.blockHeigthPx()
-        this.boardBlocksGrid = [...Array(boardWidthBlocks)].map(e => Array(boardHeigthBlocks).fill(false));
+        const boardWidthBlocks = boardHTMLElements.boardWidthPx / boardHTMLElements.blockWidthPx
+        const boardHeigthBlocks = boardHTMLElements.boardHeigthPx / boardHTMLElements.blockHeigthPx
+        this.boardBlocksGrid = [...Array(boardWidthBlocks)].map(e => Array(boardHeigthBlocks).fill({isFilled: false, block: null}));
+        this.htmlBoard = boardHTMLElements
     }
 
-    addBlock(block, x, y) {
+    addBlock(x, y) {
+        
         this.boardBlocksGrid[x][y] = true
-        // this.board.appendChild(block)
+        // return this.htmlBoard.addBlock(x, y)
     }
     
     removeBlock(x, y) {
         this.boardBlocksGrid[x][y] = false
     }
     
+    /* Check if we can move block from old pos to new pos, and move it */
     changeBlockPotision(block, oldX, oldY, newX, newY) {
+
+        // check that block doesnt exceed board limits
+        if(newX >= boardHTMLElements.boardWidthBlocks ||
+            newX < 0 ||
+             newY >= boardHTMLElements.boardHeigthBlocks ||
+             newY < 0) 
+            return false
+            
         this.boardBlocksGrid[oldX][oldY] = false
-        this.boardBlocksGrid[newX, newY] = true
+        this.boardBlocksGrid[newX][newY] = true
+        this.htmlBoard.moveBlock(block, newX, newY)
+
+        return true
     }
     
 }
 
 class Block {
-    constructor(boardBlocksGrid) {
-        
-        // this.blockElem = document.createElement("div")
-        // this.blockElem.className = blockStyle
-        
-        this.position = [0, 0]
+    constructor(boardBlocksGrid, x = 0, y = 0) {
+        this.position = [x, y]
         this.boardGrid = boardBlocksGrid
+        this.blockHTMLElem = null
     }
 
     showBlock() {
-        this.boardGrid.addBlock(this, this.position[0], this.position[1])
+        this.blockHTMLElem = this.boardGrid.addBlock(this.position[0], this.position[1])
     }
 
     setPosition(x, y) {
-        this.boardGrid.changeBlockPotision(this, this.position[0], this.position[1], x, y)
-        this.position = [x, y]
+        if(this.blockHTMLElem !== null) {
+            if(this.boardGrid.changeBlockPotision(this.blockHTMLElem,
+                 this.position[0], this.position[1], x, y))
+                this.position = [x, y]
+        }
     }
 
     moveLeft() {
-        this.setPosition(this.position[0], this.position[1] + 1)
+        this.setPosition(this.position[0] - 1, this.position[1])
     }
 
     moveRight() {
         this.setPosition(this.position[0] + 1, this.position[1])
+    }
+
+    moveUp() {
+        this.setPosition(this.position[0], this.position[1] - 1)
+    }
+
+    moveDown() {
+        this.setPosition(this.position[0], this.position[1] + 1)
     }
 
     remove() {
@@ -108,17 +135,11 @@ class Block {
     
 }
 
-const createShapeI = (boardPosition) => {
-    // i
-    let i0 = createBlock()    
-    let i1 = createBlock()
-    let i2 = createBlock()
-    let i3 = createBlock()
-
-}
 
 let elems = new boardHTMLElements("game_board", "block_shape")
-console.log(elems.boardWidthPx)
-console.log(elems.boardHeigthPx)
-console.log(elems.blockWidthPx)
-console.log(elems.blockHeigthPx)
+let grid = new boardBlocksGrid(elems)
+let block = new Block(grid, 0, 0)
+block.showBlock()
+// block.setPosition(1,1)
+block.moveLeft()
+block.moveDown()
